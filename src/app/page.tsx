cada576afff7677c3d792cc170bc603fb94a99e0
Aleydon <aleydonroberto@gmail.com>
2025-07-29 16:21:28 -0300

    chore(initial setup): first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-amber-200">
+      <h2 className="text-2xl font-bold">Hello World</h2>
+    </div>
+  );
+}
