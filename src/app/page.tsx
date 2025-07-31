37d527da42c9ca843dcd5b57234c6bb545899d96
Aleydon <aleydonroberto@gmail.com>
2025-07-31 16:15:14 -0300

    chore(ui library): setup chadcn-ui colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button';
+
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-amber-200">
-      <h2 className="text-2xl font-bold">Hello World</h2>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h2 className="text-2xl">Hello World</h2>
+      <Button variant={'default'}>Nya</Button>
     </div>
   );
 }
